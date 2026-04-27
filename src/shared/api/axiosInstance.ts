d2c14bb8d628afcbcd05214bdb7abb4f52283cef
MahmoudEmad <117545515+ma7mod7@users.noreply.github.com>
2026-04-27 03:19:55 +0300

    fix url problem

--- src/shared/api/axiosInstance.ts
+++ src/shared/api/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: '/api',    
+    baseURL: '/',    
     headers: {
         'Content-Type': 'application/json',
     },
